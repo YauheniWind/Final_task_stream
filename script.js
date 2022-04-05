05efdf0650f8266b8a835a14e0cab24a7fafdf61
YauheniWind <78211102+YauheniWind@users.noreply.github.com>
2022-04-05 14:05:17 +0200

    add final task stream

---
+++ script.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+const ytdl = require('ytdl-core');
+const ffmpeg = require('fluent-ffmpeg');
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+ffmpeg.setFfmpegPath(ffmpegPath);
+
+
+const musicUrl = process.argv[2].split(',');
+
+(async () => {
+    for await (let link of musicUrl) {
+        const readableVideoStream = ytdl(link)
+        const outputPath = path.resolve(`/Users/evgenijgravdin/Desktop/Final_task_stream/music/music_` + `${Math.random()}` + `.mp3`)
+
+        ffmpeg(readableVideoStream)
+            .withNoVideo()
+            .toFormat('mp3')
+            .saveToFile(outputPath)
+    }
+})()
+
+musicUrl.forEach(inputImagePath => {
+    ytdl(inputImagePath).pipe(fs.createWriteStream(`${process.argv[3]}_${Math.random()}.mp4`))
+})
